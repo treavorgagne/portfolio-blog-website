5ad8fb05193fb27b7d246d65afdcfeb6c1e4b183
Treavor Gagne <50676825+treavorgagne@users.noreply.github.com>
2025-04-23 22:57:22 -0700

    fix layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,22 +19,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
